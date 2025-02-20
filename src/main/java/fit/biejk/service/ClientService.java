4cd7c4da4bf58740f46c5cd84b27ebf2697c3913
Strogolsky <strogolsky@gmail.com>
2025-02-20 18:34:12 +0100

    feat #5 : implement basic Client service

---
+++ src/main/java/fit/biejk/service/ClientService.java
@@ -0,0 +1,49 @@
+package fit.biejk.service;
+
+import fit.biejk.entity.Client;
+import fit.biejk.repository.ClientRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@ApplicationScoped
+@NoArgsConstructor
+public class ClientService {
+
+    @Inject
+    ClientRepository clientRepository;
+
+    public Client create(Client client) {
+        clientRepository.persist(client);
+        return client;
+    }
+
+    public List<Client> getAll() {
+        return clientRepository.listAll();
+    }
+
+    public Client getById(Long id) {
+        return clientRepository.findById(id);
+    }
+
+    public Client update(Client client) {
+        Client old = clientRepository.findById(client.getId());
+        if(old == null) {
+            // todo
+        }
+        old.setFirstName(client.getFirstName());
+        old.setLastName(client.getLastName());
+        clientRepository.persist(old);
+        return old;
+    }
+
+    public void delete(Long id) {
+        Client client = clientRepository.findById(id);
+        clientRepository.delete(client);
+    }
+
+
+
+}

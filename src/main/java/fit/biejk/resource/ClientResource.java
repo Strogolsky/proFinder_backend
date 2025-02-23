79025ef0278c576a6af74842e436677de5b61e54
Strogolsky <strogolsky@gmail.com>
2025-02-23 13:47:03 +0100

    feat #5 : implement endpoints ClientResource

--- src/main/java/fit/biejk/resource/ClientResource.java
+++ src/main/java/fit/biejk/resource/ClientResource.java
@@ -3,9 +3,7 @@ package fit.biejk.resource;
 import fit.biejk.entity.Client;
 import fit.biejk.service.ClientService;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
 import java.util.List;
@@ -22,9 +20,30 @@ public class ClientResource {
         return Response.ok(result).build();
     }
 
+    @GET
+    @Path("/{id}")
+    public Response getById(@PathParam("id") Long id) {
+        return Response.ok(clientService.getById(id)).build();
+
+    }
+
     @POST
     public Response create(Client client) {
         Client result = clientService.create(client);
         return Response.ok(result).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") Long id, Client client) {
+        Client result = clientService.update(client);
+        return Response.ok(result).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") Long id) {
+        clientService.delete(id);
+        return Response.ok().build();
     }
 }

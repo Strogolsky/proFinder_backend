02e4718266e3f894d74d6ca66537d7521048629d
Strogolsky <strogolsky@gmail.com>
2025-03-23 13:39:17 +0100

    fix #4 : checkstyle for dto

--- src/main/java/fit/biejk/dto/ClientDto.java
+++ src/main/java/fit/biejk/dto/ClientDto.java
@@ -9,27 +9,69 @@ import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
 
+/**
+ * Data Transfer Object representing a client.
+ * <p>
+ * Contains details about the client, such as personal information,
+ * contact details, and account creation timestamp.
+ * </p>
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientDto {
+
+    /**
+     * Maximum length for firstName and lastName fields.
+     */
+    public static final int NAME_MAX_LENGTH = 50;
+
+    /**
+     * Maximum length for phoneNumber field.
+     */
+    public static final int PHONE_MAX_LENGTH = 20;
+
+    /**
+     * Unique identifier of the client.
+     */
     private Long id;
 
+    /**
+     * Email address of the client, must be a valid email format.
+     */
     @Email
     private String email;
 
-    @Length(max = 50)
+    /**
+     * First name of the client, maximum length of 50 characters.
+     */
+    @Length(max = NAME_MAX_LENGTH)
     private String firstName;
 
-    @Length(max = 50)
+    /**
+     * Last name of the client, maximum length of 50 characters.
+     */
+    @Length(max = NAME_MAX_LENGTH)
     private String lastName;
 
+    /**
+     * Client's password. (Temporary field, remove when creating response.)
+     */
     private String password; // todo delete, when create response
 
-    @Length(max = 20)
+    /**
+     * Phone number of the client, maximum length of 20 characters.
+     */
+    @Length(max = PHONE_MAX_LENGTH)
     private String phoneNumber;
 
+    /**
+     * Geographical location associated with the client.
+     */
     private Location location;
 
+    /**
+     * Timestamp indicating when the client account was created.
+     */
     private LocalDateTime createAt;
 }
--- src/main/java/fit/biejk/dto/SpecialistDto.java
+++ src/main/java/fit/biejk/dto/SpecialistDto.java
@@ -12,33 +12,83 @@ import org.hibernate.validator.constraints.Length;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Data Transfer Object representing a specialist.
+ * <p>
+ * Contains details about a specialist including contact info, location,
+ * specialization, description, and availability schedule.
+ * </p>
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SpecialistDto {
+    /**
+     * Maximum length for firstName and lastName fields.
+     */
+    public static final int NAME_MAX_LENGTH = 50;
+
+    /**
+     * Maximum length for phoneNumber field.
+     */
+    public static final int PHONE_MAX_LENGTH = 20;
+
+    /**
+     * Unique identifier of the specialist.
+     */
     private Long id;
 
+    /**
+     * Specialist's email address, must be a valid email format.
+     */
     @Email
     private String email;
 
-    @Length(max = 50)
+    /**
+     * First name of the specialist, maximum length of 50 characters.
+     */
+    @Length(max = NAME_MAX_LENGTH)
     private String firstName;
 
-    @Length(max = 50)
+    /**
+     * Last name of the specialist, maximum length of 50 characters.
+     */
+    @Length(max = NAME_MAX_LENGTH)
     private String lastName;
 
+    /**
+     * Specialist's password. (Temporary field, remove when creating response.)
+     */
     private String password; // todo delete, when create response
 
-    @Length(max = 20)
+    /**
+     * Phone number of the specialist, maximum length of 20 characters.
+     */
+    @Length(max = PHONE_MAX_LENGTH)
     private String phoneNumber;
 
+    /**
+     * Geographical location of the specialist.
+     */
     private Location location;
 
+    /**
+     * Timestamp indicating when the specialist's account was created.
+     */
     private LocalDateTime createAt;
 
+    /**
+     * Specialist's professional specialization.
+     */
     private Specialization specialization;
 
+    /**
+     * Detailed description of the specialist's skills or services offered.
+     */
     private String description;
 
+    /**
+     * List of available time slots for appointments.
+     */
     private List<TimeSlot> schedule;
 }
